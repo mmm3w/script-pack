24cbef7cfb24cffe3ffe2d049e6f0692dfc1bfa8
wangzc <wangzc@zcitc.com>
2019-07-15 13:33:24 +0800

    添加服务器简单测试脚本

--- proxy-gateway/README.md
+++ proxy-gateway/README.md
@@ -2,4 +2,6 @@ base-component:基础组件安装，其中包括ssr编译库，ssr，ss-tproxy
 
 ssr-subscribe:通过订阅地址获取相关服务器的json配置文件
 
-set-server:修改ss-tproxy相关配置信息，主要用于服务器配置切换
+server-set:修改ss-tproxy相关配置信息，主要用于服务器配置切换
+
+server-test:简单测试服务器访问延迟
---
+++ proxy-gateway/server-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function traversing-test() {
+  for conf in `ls $1`
+  do
+    if [[ $conf =~ $2 ]]; then
+      url=${conf%.*}
+      time="$( ping $url -c 4 | grep "^rtt" )"
+      avg=${time##*=}
+      avg=${avg#*/}
+      avg=${avg%%/*}
+      printf "%-32s %-10s\n" $url $avg"ms"
+    fi
+  done
+}
+
+traversing-test $1 $2
